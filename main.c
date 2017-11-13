601f6735f9e3e760b760baae490bb653d78f973b
fabiola <fradosav@stuy.edu>
2017-11-12 23:52:12 -0500

    hw

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include 
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+char ** parse_args(char * line){
+  char ** stuff = (char **)calloc(6, sizeof(char *));
+  int i = 0;
+  while (line){
+    stuff[i] = strsep(&line, " ");
+    i++;
+  }
+  return stuff;
+}
 
 int main(){
-
-
+  char line[52] = "ls -a -l";
+  char ** stuff = parse_args(line);
+  execvp(stuff[0], stuff);
 }
+
